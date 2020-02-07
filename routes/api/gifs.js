209b6cb6c40d882ce2f49887aeb961b86ee70a70
NaweedSufranj <sufranj.naweed@gmail.com>
2020-02-07 21:47:08 +0100

    Refactoring of Stickers & Gifs Routes

--- routes/api/gifs.js
+++ routes/api/gifs.js
@@ -12,15 +12,24 @@ const tools             = require('../../libs/tools');
 // @access  Public
 router.get('/',  async (req, res) => {
     const API_URL = `http://api.giphy.com/v1/gifs/trending?&api_key=${api_key}`;
-    return res.send(await tools.fetcher(API_URL));
+    const data = await tools.fetcher(API_URL);
+    if(data)
+        return res.status(200).send(data);
+    else
+        return res.status(404).send('ressources not found');
 });
 
 // @route   GET api/v1/search
 // @desc    search gifs by title
 // @access  Public
 router.get('/search/:wordSearch', async (req, res)=>{
+    const search    = req.params.wordSearch;
     const API_URL = `http://api.giphy.com/v1/gifs/search?q=${search}&api_key=${api_key}`;
-    return res.send(await tools.fetcher(API_URL));
+    const data = await tools.fetcher(API_URL);
+    if(data)
+        return res.status(200).send(data);
+    else
+        return res.status(404).send('ressources not found');
 });
 
 // @route   GET api/v1/gifs/random
@@ -28,8 +37,25 @@ router.get('/search/:wordSearch', async (req, res)=>{
 // @access  Public
 router.get('/random', async(req, res) =>{
     const API_URL = `http://api.giphy.com/v1/gifs/random?&api_key=${api_key}`;
-    return res.send(await tools.fetcher(API_URL));
+    const data = await tools.fetcher(API_URL);
+    if(data)
+        return res.status(200).send(data);
+    else
+        return res.status(404).send('ressources not found');
 });
 
+// @route   GET api/v1/gifs/search?q=MY_SEARCH
+// @desc    search gif by id
+// @access  Public
+router.get('/search/byID/:idSearch', async (req, res)=>{
+    const id    = req.params.idSearch;
+    const API_URL = `http://api.giphy.com/v1/gifs/${id}/&api_key=${api_key}`;
+    console.log(API_URL);
+    const data = await tools.fetcher(API_URL);
+    if(data)
+        return res.status(200).send(data);
+    else
+        return res.status(404).send('ressources not found');
+});
 
 module.exports  = router;
--- routes/api/stickers.js
+++ routes/api/stickers.js
@@ -4,12 +4,42 @@ const router            = express.Router();
 const api_key           = process.env.API_KEY;
 const tools             = require('../../libs/tools');
 
+// @route   GET api/v1/stickers
+// @desc    Get 25 trend's Stickers
+// @access  Public
+router.get('/',  async (req, res) => {
+    const API_URL = `http://api.giphy.com/v1/stickers/trending?&api_key=${api_key}`;
+    const data = await tools.fetcher(API_URL);
+    if(data)
+        return res.status(200).send(data);
+    else
+        return res.status(404).send('ressources not found');
+});
+
 // @route  api/v1/stickers/random
-// @desc   Get a random stickers
+// @desc   Get a random sticker
 // @access Public
-router.get('/', async (req, res) => {
+router.get('/random', async (req, res) => {
     const API_URL = `http://api.giphy.com/v1/stickers/random?&api_key=${api_key}`
-    return res.send(await tools.fetcher(API_URL));
+    const data = await tools.fetcher(API_URL);
+    if(data)
+        return res.status(200).send(data);
+    else
+        return res.status(404).send('ressources not found');
 })
+
+// @route   GET api/v1/stickers/search?q=MY_SEARCH
+// @desc    search stickers by title
+// @access  Public
+router.get('/search/:wordSearch', async (req, res)=>{
+    const search    = req.params.wordSearch;
+    const API_URL = `http://api.giphy.com/v1/stickers/search?q=${search}&api_key=${api_key}`;
+    const data = await tools.fetcher(API_URL);
+    if(data)
+        return res.status(200).send(data);
+    else
+        return res.status(404).send('ressources not found');
+});
+
 
 module.exports  = router;
