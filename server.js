ffacbb7990d991fcd6dd48f00260806295f3b3f5
NaweedSufranj <sufranj.naweed@gmail.com>
2020-02-24 14:04:29 +0100

    remove spacing

--- server.js
+++ server.js
@@ -7,7 +7,6 @@ const PORT              = process.env.PORT || 8080;
 const HOST              = '0.0.0.0';
 const cors              = require("cors");
 
-
 app.use(bodyParser.json());
 // Init Middleware
 app.use(express.json({ extended : false }));
@@ -16,16 +15,14 @@ app.use(express.json({ extended : false }));
 app.use(cors());
 
 app.get('/', (req, res) => {
-    res.status(200).send(`Giphy Fetcher API is Running - http://${HOST}:${PORT}`);
+    res.status(200).send(`- Giphy Fetcher API is Running -`);
 });
 
 app.use('/api/v1/gifs', require('./routes/api/gifs'));
 app.use('/api/v1/stickers', require('./routes/api/stickers'));
  
-
 //app.listen(PORT, HOST);
 //console.log(`Running on http://${HOST}:${PORT}`);
-
 // Initialize the app.
 const server = app.listen(PORT, function () {
     const port = server.address().port;
